21f7b4678473efe4c6dafd3a3e153335b7b84aba
Ned Zimmerman <ned@bight.ca>
2016-09-22 14:51:00 -0400

    Add filter to support custom post types.
    Document functions and refactor logic for display.

--- formgen.php
+++
@@ -1,7 +0,0 @@
-<div class="wrap">
-<form method="post" action="options.php">
-      <?php echo settings_fields( "my_option_group" ); ?>
-      <?php echo do_settings_sections( "hypothesis-setting-admin" ); ?>
-      <?php echo submit_button(); ?>
-</form>
-</div>
