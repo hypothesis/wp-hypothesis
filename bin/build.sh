3d684301f315dfc15cff5c901a39a4f6028a8fda
Ned Zimmerman <ned@bight.ca>
2019-08-09 22:45:05 -0300

    Don't use dist-archive

--- bin/build.sh
+++ bin/build.sh
@@ -4,11 +4,12 @@ PLUGIN="hypothesis"
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 PLUGIN_BUILDS_PATH="$PROJECT_ROOT/builds"
 VERSION=$(php -f "$PROJECT_ROOT/bin/get_plugin_version.php" "$PROJECT_ROOT" "$PLUGIN")
-ZIP_FILE="$PLUGIN_BUILDS_PATH/$PLUGIN-$VERSION.zip"
 
 cd "$PROJECT_ROOT"
 [ -d "$PLUGIN_BUILDS_PATH" ] || mkdir "$PLUGIN_BUILDS_PATH"
 
 wp i18n make-pot . "languages/hypothesis.pot"
 
-wp dist-archive . "$ZIP_FILE"
+mkdir "$PLUGIN_BUILDS_PATH/$PLUGIN-$VERSION"
+
+rsync -r --exclude-from .distignore $PROJECT_ROOT/ "$PLUGIN_BUILDS_PATH/$PLUGIN-$VERSION"
